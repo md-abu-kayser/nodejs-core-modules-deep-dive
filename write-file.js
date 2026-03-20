563d210826563e7a04078eb20e3f46352b6283bd
md-abu-kayser <abu.kayser.official@gmail.com>
2026-03-21 00:07:39 +0600

    feat(fs): implement synchronous file writing using fs module

--- write-file.js
+++ write-file.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
-const content1 = "This is a content /n Synchronous";
+const content1 = "This is a content \n Synchronous";
 
 try {
+  fs.writeFileSync("./output/test-sync.txt", content1);
+  console.log("file written sync");
 } catch (err) {
   console.err(err.message);
 }
