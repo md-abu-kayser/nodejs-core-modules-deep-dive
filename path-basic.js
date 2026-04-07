b979d9df102a9f9ccdaf0e1f8296534bf8e94708
md-abu-kayser <abu.kayser.official@gmail.com>
2026-04-07 16:13:01 +0600

    feat: add path module examples for file path analysis and parsing
    - demonstrated usage of __filename and __dirname
    - implemented path methods (dirname, basename, extname)
    - added path.parse() for structured path breakdown
    - used path.format() to reconstruct file path

--- path-basic.js
+++ path-basic.js
@@ -1,5 +1,22 @@
 const path = require("path");
 
-console.log("Current file name:\n");
+console.log("Current File Info:\n");
 console.log("filename :", __filename);
 console.log("directory :", __dirname);
+
+console.log("\n" + "-".repeat(50) + "\n");
+const filePath = "/kayser/documents/nextLevel.pdf";
+
+console.log("analyzing Path :", filePath, "\n");
+console.log("Directory:", path.dirname(filePath));
+console.log("Base name:", path.basename(filePath));
+console.log("File Extension:", path.extname(filePath));
+console.log("File name:", path.basename(filePath, path.extname(filePath)));
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const parsed = path.parse(filePath);
+console.log("Parsed path object:", parsed);
+
+console.log("\n" + "-".repeat(50) + "\n");
+console.log("Formatted path:", path.format(parsed));
