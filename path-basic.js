e32cd24b66aabc87d860b90a38006f4eb6b75e59
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-03 23:32:06 +0600

    feat: basic path add

--- path-basic.js
+++ path-basic.js
@@ -3,3 +3,13 @@ const path = require("path");
 console.log("Current file info: \n");
 console.log("filename: ", __filename);
 console.log("Directory: ", __dirname);
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const filePath = "/kayser/document/nextLevel.pdf";
+console.log("Analyzing path :", filePath, "\n");
+
+console.log("Directory name : ", path.dirname(filePath));
+console.log("Base name :", path.basename(filePath));
+console.log("File Extension :", path.extname(filePath));
+console.log("File Name :", path.basename(filePath, path.extname(filePath)));
