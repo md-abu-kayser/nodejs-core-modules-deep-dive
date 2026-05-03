5e8dbfd1358737fe6a41006d92be622384118ca3
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-04 00:20:17 +0600

    feat: explore Node.js path module for file path analysis

--- path-basic.js
+++ path-basic.js
@@ -13,3 +13,12 @@ console.log("Directory name : ", path.dirname(filePath));
 console.log("Base name :", path.basename(filePath));
 console.log("File Extension :", path.extname(filePath));
 console.log("File Name :", path.basename(filePath, path.extname(filePath)));
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const parsed = path.parse(filePath);
+console.log("Parsed path object: ", parsed);
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+console.log("formatted path :", path.format(parsed));
